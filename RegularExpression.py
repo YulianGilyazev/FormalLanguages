7eb4944465c7b45a9f4e0fb4f8b281204dfdb56e
YulianGilyazev <45824886+YulianGilyazev@users.noreply.github.com>
2019-11-05 14:03:55 +0300

    Add files via upload

---
+++ RegularExpression.py
@@ -0,0 +1,114 @@
+import string
+import numpy as np
+
+
+def fnd_mx(s, u):
+    st = list()
+    for cur_symb in s:
+        if cur_symb.isalpha():
+            dp = np.zeros([len(u), len(u)])
+            dp_suf = np.zeros([len(u), len(u)])
+            dp_pref = np.zeros([len(u), len(u)])
+            dp_in = np.zeros([len(u), len(u)])
+            for i in range(len(u)):
+                if u[i] == cur_symb:
+                    dp[i][i] = True
+                    dp_suf[i][i] = True
+                    dp_pref[i][i] = True
+                    dp_in[i][i] = True
+            st.append([0, dp, dp_suf, dp_pref, dp_in])
+
+        if cur_symb == '*':
+            arg = st[-1]
+            cnt = len(u) + 10
+            while cnt > 0:
+                cnt -= 1
+                for i in range(len(u)):
+                    for j in range(i, len(u)):
+                        for k in range(i, j):
+                            if arg[1][i][k] and arg[1][k + 1][j]:
+                                arg[1][i][j] = True
+                            if arg[1][i][k] and arg[3][k + 1][j]:
+                                arg[3][i][j] = True
+                            if arg[2][i][k] and arg[1][k + 1][j]:
+                                arg[2][i][j] = True
+                            if arg[2][i][k] and arg[3][k + 1][j]:
+                                arg[4][i][j] = True
+            st.pop(-1)
+            arg[0] = True
+            st.append(arg)
+
+        if cur_symb == '+':
+            if len(st) < 2:
+                raise Exception("IncorrectRegularExpression")
+            left_arg = st[-2]
+            right_arg = st[-1]
+            for i in range(len(u)):
+                for j in range(len(u)):
+                    left_arg[1][i][j] = max([left_arg[1][i][j], right_arg[1][i][j]])
+                    left_arg[2][i][j] = max([left_arg[2][i][j], right_arg[2][i][j]])
+                    left_arg[3][i][j] = max([left_arg[3][i][j], right_arg[3][i][j]])
+                    left_arg[4][i][j] = max([left_arg[4][i][j], right_arg[4][i][j]])
+            st.pop(-1)
+            st.pop(-1)
+            left_arg[0] = max([left_arg[0], right_arg[0]])
+            st.append(left_arg)
+
+        if cur_symb == '.':
+            if len(st) < 2:
+                raise Exception("IncorrectRegularExpression")
+            left_arg = st[-2]
+            right_arg = st[-1]
+            ans = np.zeros((len(u), len(u)))
+            ans_suf = np.zeros((len(u), len(u)))
+            ans_pref = np.zeros((len(u), len(u)))
+            ans_in = np.zeros((len(u), len(u)))
+            for i in range(len(u)):
+                for j in range(i, len(u)):
+                    if left_arg[0] and right_arg[1][i][j]:
+                        ans[i][j] = True
+                    if right_arg[0] and left_arg[1][i][j]:
+                        ans[i][j] = True
+                    if left_arg[3][i][j]:
+                        ans_pref[i][j] = True
+                    if right_arg[3][i][j] and left_arg[0]:
+                        ans_pref[i][j] = True
+                    if right_arg[2][i][j]:
+                        ans_suf[i][j] = True
+                    if left_arg[2][i][j] and right_arg[0]:
+                        ans_suf[i][j] = True
+                    if right_arg[4][i][j]:
+                        ans_in[i][j] = True
+                    if left_arg[4][i][j]:
+                        ans_in[i][j] = True
+                    for k in range(i, j):
+                        if left_arg[1][i][k] and right_arg[1][k + 1][j]:
+                            ans[i][j] = True
+                        if left_arg[1][i][k] and right_arg[3][k + 1][j]:
+                            ans_pref[i][j] = True
+                        if left_arg[2][i][k] and right_arg[1][k + 1][j]:
+                            ans_suf[i][j] = True
+                        if left_arg[2][i][k] and right_arg[3][k + 1][j]:
+                            ans_in[i][j] = True
+            st.pop(-1)
+            st.pop(-1)
+            st.append([(left_arg[0] or right_arg[0]), ans, ans_suf, ans_pref, ans_in])
+    if len(st) > 1:
+        raise Exception("IncorrectRegularExpression")
+    table = st[0][4]
+    mx_sz = 0
+    for i in range(len(u)):
+        for j in range(len(u)):
+            if j - i + 1 > mx_sz and table[i, j]:
+                mx_sz = j - i + 1
+    return mx_sz
+
+
+def main():
+    inp = input()
+    s, u = inp.split()
+    print(fnd_mx(s, u))
+
+
+if __name__ == '__main__':
+    main()
---
+++ TESTS.py
@@ -0,0 +1,25 @@
+import unittest
+from RegularExpression import fnd_mx
+
+
+class TestUM(unittest.TestCase):
+    def test_exception(self):
+        self.assertRaises(BaseException, fnd_mx, "ab.+", "abc")
+        self.assertRaises(BaseException, fnd_mx, "a*+", "abc")
+        self.assertRaises(BaseException, fnd_mx, "ab.b*c+", "abc")
+        self.assertRaises(BaseException, fnd_mx, "acb..bab.c.*ab.ba.+.+*a.", "abbaa")
+        self.assertRaises(BaseException, fnd_mx, "ab+c.aba.*+.bac.+.+*", "babc")
+
+    def test_main(self):
+        self.assertEqual(5, fnd_mx("acb..bab.c.*.ab.ba.+.+*a.", "abbaa"))
+        self.assertEqual(3, fnd_mx("ab+c.aba.*.bac.+.+*", "babc"))
+        self.assertEqual(3, fnd_mx("ab.ba.+*", "bab"))
+        self.assertEqual(8, fnd_mx("ab.cb.+ab+*cb.+ac.+.ab.ca.+*b.a.ab*+.+", "abaaabbbcbabc"))
+        self.assertEqual(7, fnd_mx("ab.cb.+ab+*cb.+ac.+.ab.ca.+*b.a.ab*+.+", "cbaabba"))
+        self.assertEqual(2, fnd_mx("ab*.", "bab"))
+        self.assertEqual(11, fnd_mx("a*b*.", "bbbbbaaaaabbbbbbaaaabbbb"))
+        self.assertEqual(1, fnd_mx("ab.", "bbbbb"))
+
+
+if __name__ == '__main__':
+    unittest.main()
