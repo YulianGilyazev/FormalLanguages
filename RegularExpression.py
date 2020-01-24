44782db5420bcfd4fc93b0d048e6b75a998d8900
YulianGilyazev <45824886+YulianGilyazev@users.noreply.github.com>
2020-01-24 23:52:02 +0300

    Add files via upload

--- RegularExpression.py
+++ RegularExpression.py
@@ -1,148 +1,136 @@
 import string
-
 import numpy as np
 
+def concatenate(st, u):
+    if len(st) < 2:
+        raise Exception("IncorrectRegularExpression")
+    left_arg = st[-2]
+    right_arg = st[-1]
+    ans = np.zeros((len(u), len(u)))
+    ans_suf = np.zeros((len(u), len(u)))
+    ans_pref = np.zeros((len(u), len(u)))
+    ans_in = np.zeros((len(u), len(u)))
+    for i in range(len(u)):
+        for j in range(i, len(u)):
+            if left_arg[0] and right_arg[1][i][j]:
+                ans[i][j] = True
+            if right_arg[0] and left_arg[1][i][j]:
+                ans[i][j] = True
+            if left_arg[3][i][j]:
+                ans_pref[i][j] = True
+            if right_arg[3][i][j] and left_arg[0]:
+                ans_pref[i][j] = True
+            if right_arg[2][i][j]:
+                ans_suf[i][j] = True
+            if left_arg[2][i][j] and right_arg[0]:
+                ans_suf[i][j] = True
+            if right_arg[4][i][j]:
+                ans_in[i][j] = True
+            if left_arg[4][i][j]:
+                ans_in[i][j] = True
+            for k in range(i, j):
+                if (left_arg[1][i][k] and right_arg[1][k + 1][j]):
+                    ans[i][j] = True
+                if (left_arg[1][i][k] and right_arg[3][k + 1][j]):
+                    ans_pref[i][j] = True
+                if (left_arg[2][i][k] and right_arg[1][k + 1][j]):
+                    ans_suf[i][j] = True
+                if (left_arg[2][i][k] and right_arg[3][k + 1][j]):
+                    ans_in[i][j] = True
+    st.pop(-1)
+    st.pop(-1)
+    st.append([(left_arg[0] and right_arg[0]), ans, ans_suf, ans_pref, ans_in,])
 
-def fnd_mx(s, u):
-    st = list()
+
+def unite(st, u):
+    if len(st) < 2:
+        raise Exception("IncorrectRegularExpression")
+    left_arg = st[-2]
+    right_arg = st[-1]
+    for i in range(len(u)):
+        for j in range(len(u)):
+            left_arg[1][i][j] = max([left_arg[1][i][j], right_arg[1][i][j]])
+            left_arg[2][i][j] = max([left_arg[2][i][j], right_arg[2][i][j]])
+            left_arg[3][i][j] = max([left_arg[3][i][j], right_arg[3][i][j]])
+            left_arg[4][i][j] = max([left_arg[4][i][j], right_arg[4][i][j]])
+    st.pop(-1)
+    st.pop(-1)
+    left_arg[0] = max([left_arg[0], right_arg[0]])
+    st.append(left_arg)
+
+
+def add_empty_string_re(st, u):
+    dp = np.zeros([len(u), len(u)])
+    dp_suf = np.zeros([len(u), len(u)])
+    dp_pref = np.zeros([len(u), len(u)])
+    dp_in = np.zeros([len(u), len(u)])
+    st.append([1, dp, dp_suf, dp_pref, dp_in])
+
+
+def add_one_symbol_re(st, u, cur_symb):
+    dp = np.zeros([len(u), len(u)])
+    dp_suf = np.zeros([len(u), len(u)])
+    dp_pref = np.zeros([len(u), len(u)])
+    dp_in = np.zeros([len(u), len(u)])
+    for i in range(len(u)):
+        if u[i] == cur_symb:
+            dp[i][i] = True
+            dp_suf[i][i] = True
+            dp_pref[i][i] = True
+            dp_in[i][i] = True
+    st.append([0, dp, dp_suf, dp_pref, dp_in])
+
+
+def closure(st, u):
+    arg = st[-1]
+    cnt = len(u) + 10
+    while cnt > 0:
+        cnt -= 1
+        for i in range(len(u)):
+            for j in range(i, len(u)):
+                for k in range(i, j):
+                    if arg[1][i][k] and arg[1][k + 1][j]:
+                        arg[1][i][j] = True
+                    if arg[1][i][k] and arg[3][k + 1][j]:
+                        arg[3][i][j] = True
+                    if arg[2][i][k] and arg[1][k + 1][j]:
+                        arg[2][i][j] = True
+                    if arg[2][i][k] and arg[3][k + 1][j]:
+                        arg[4][i][j] = True
+    st.pop(-1)
+    arg[0] = True
+    st.append(arg)
+
+
+def find_max_substring(s, u):
+    st = []
     for cur_symb in s:
         if cur_symb.isalpha():
-            dp = np.zeros([len(u), len(u)])
-            dp_suf = np.zeros([len(u), len(u)])
-            dp_pref = np.zeros([len(u), len(u)])
-            dp_in = np.zeros([len(u), len(u)])
-            for i in range(len(u)):
-                if u[i] == cur_symb:
-                    dp[i][i] = True
-                    dp_suf[i][i] = True
-                    dp_pref[i][i] = True
-                    dp_in[i][i] = True
-            st.append([0, dp, dp_suf, dp_pref, dp_in])
+            add_one_symbol_re(st, u, cur_symb)
         if cur_symb == "1":
-            dp = np.zeros([len(u), len(u)])
-            dp_suf = np.zeros([len(u), len(u)])
-            dp_pref = np.zeros([len(u), len(u)])
-            dp_in = np.zeros([len(u), len(u)])
-            st.append([1, dp, dp_suf, dp_pref, dp_in])
+            add_empty_string_re(st, u)
         if cur_symb == "*" or cur_symb == "∗":
-            arg = st[-1]
-            cnt = len(u) + 10
-            while cnt > 0:
-                cnt -= 1
-                for i in range(len(u)):
-                    for j in range(i, len(u)):
-                        for k in range(i, j):
-                            if arg[1][i][k] and arg[1][k + 1][j]:
-                                arg[1][i][j] = True
-                            if arg[1][i][k] and arg[3][k + 1][j]:
-                                arg[3][i][j] = True
-                            if arg[2][i][k] and arg[1][k + 1][j]:
-                                arg[2][i][j] = True
-                            if arg[2][i][k] and arg[3][k + 1][j]:
-                                arg[4][i][j] = True
-            st.pop(-1)
-            arg[0] = True
-            st.append(arg)
-
+            closure(st, u)
         if cur_symb == "+":
-            if len(st) < 2:
-                raise Exception("IncorrectRegularExpression")
-            left_arg = st[-2]
-            right_arg = st[-1]
-            for i in range(len(u)):
-                for j in range(len(u)):
-                    left_arg[1][i][j] = max(
-                        [left_arg[1][i][j], right_arg[1][i][j]]
-                    )
-                    left_arg[2][i][j] = max(
-                        [left_arg[2][i][j], right_arg[2][i][j]]
-                    )
-                    left_arg[3][i][j] = max(
-                        [left_arg[3][i][j], right_arg[3][i][j]]
-                    )
-                    left_arg[4][i][j] = max(
-                        [left_arg[4][i][j], right_arg[4][i][j]]
-                    )
-            st.pop(-1)
-            st.pop(-1)
-            left_arg[0] = max([left_arg[0], right_arg[0]])
-            st.append(left_arg)
-
+            unite(st, u)
         if cur_symb == ".":
-            if len(st) < 2:
-                raise Exception("IncorrectRegularExpression")
-            left_arg = st[-2]
-            right_arg = st[-1]
-            ans = np.zeros((len(u), len(u)))
-            ans_suf = np.zeros((len(u), len(u)))
-            ans_pref = np.zeros((len(u), len(u)))
-            ans_in = np.zeros((len(u), len(u)))
-            for i in range(len(u)):
-                for j in range(i, len(u)):
-                    if left_arg[0] and right_arg[1][i][j]:
-                        ans[i][j] = True
-                    if right_arg[0] and left_arg[1][i][j]:
-                        ans[i][j] = True
-                    if left_arg[3][i][j]:
-                        ans_pref[i][j] = True
-                    if right_arg[3][i][j] and left_arg[0]:
-                        ans_pref[i][j] = True
-                    if right_arg[2][i][j]:
-                        ans_suf[i][j] = True
-                    if left_arg[2][i][j] and right_arg[0]:
-                        ans_suf[i][j] = True
-                    if right_arg[4][i][j]:
-                        ans_in[i][j] = True
-                    if left_arg[4][i][j]:
-                        ans_in[i][j] = True
-                    for k in range(i, j):
-                        if (
-                            left_arg[1][i][k]
-                            and right_arg[1][k + 1][j]
-                        ):
-                            ans[i][j] = True
-                        if (
-                            left_arg[1][i][k]
-                            and right_arg[3][k + 1][j]
-                        ):
-                            ans_pref[i][j] = True
-                        if (
-                            left_arg[2][i][k]
-                            and right_arg[1][k + 1][j]
-                        ):
-                            ans_suf[i][j] = True
-                        if (
-                            left_arg[2][i][k]
-                            and right_arg[3][k + 1][j]
-                        ):
-                            ans_in[i][j] = True
-            st.pop(-1)
-            st.pop(-1)
-            st.append(
-                [
-                    (left_arg[0] and right_arg[0]),
-                    ans,
-                    ans_suf,
-                    ans_pref,
-                    ans_in,
-                ]
-            )
+            concatenate(st, u)
     if len(st) > 1:
         raise Exception("IncorrectRegularExpression")
     table = st[0][4]
-    mx_sz = 0
+    max_size = 0
     if u == "1":
         return 0
     for i in range(len(u)):
         for j in range(len(u)):
-            if j - i + 1 > mx_sz and table[i, j]:
-                mx_sz = j - i + 1
-    return mx_sz
+            if j - i + 1 > max_size and table[i, j]:
+                max_size = j - i + 1
+    return max_size
 
 
 def main():
     s, u = input().split()
-    print(fnd_mx(s, u))
+    print(find_max_substring(s, u))
 
 
 if __name__ == "__main__":
--- TESTS.py
+++ TESTS.py
@@ -1,33 +1,34 @@
 import unittest
-from RegularExpression import fnd_mx
+from RegularExpression import find_max_substring
 
 
 class TestUM(unittest.TestCase):
     def test_exception(self):
-        self.assertRaises(BaseException, fnd_mx, "ab.+", "abc")
-        self.assertRaises(BaseException, fnd_mx, "a*+", "abc")
-        self.assertRaises(BaseException, fnd_mx, "ab.b*c+", "abc")
-        self.assertRaises(BaseException, fnd_mx, "acb..bab.c.*ab.ba.+.+*a.", "abbaa")
-        self.assertRaises(BaseException, fnd_mx, "ab+c.aba.*+.bac.+.+*", "babc")
+        self.assertRaises(BaseException, find_max_substring, "ab.+", "abc")
+        self.assertRaises(BaseException, find_max_substring, "a*+", "abc")
+        self.assertRaises(BaseException, find_max_substring, "ab.b*c+", "abc")
+        self.assertRaises(BaseException, find_max_substring, "acb..bab.c.*ab.ba.+.+*a.", "abbaa")
+        self.assertRaises(BaseException, find_max_substring, "ab+c.aba.*+.bac.+.+*", "babc")
 
     def test_main(self):
-        self.assertEqual(5, fnd_mx("acb..bab.c.*.ab.ba.+.+*a.", "abbaa"))
-        self.assertEqual(3, fnd_mx("ab+c.aba.*.bac.+.+*", "babc"))
-        self.assertEqual(3, fnd_mx("ab.ba.+*", "bab"))
-        self.assertEqual(8, fnd_mx("ab.cb.+ab+*cb.+ac.+.ab.ca.+*b.a.ab*+.+", "abaaabbbcbabc"))
-        self.assertEqual(7, fnd_mx("ab.cb.+ab+*cb.+ac.+.ab.ca.+*b.a.ab*+.+", "cbaabba"))
-        self.assertEqual(2, fnd_mx("ab*.", "bab"))
-        self.assertEqual(11, fnd_mx("a*b*.", "bbbbbaaaaabbbbbbaaaabbbb"))
-        self.assertEqual(1, fnd_mx("ab.", "bbbbb"))
-        self.assertEqual(9, fnd_mx("ab*.1+*a*b.1+*+", "abbbababa"))
-        self.assertEqual(5, fnd_mx("ab*a.+1ab.+.", "bbaab"))
-        self.assertEqual(6, fnd_mx("ab.a.1+b*a.b+.1ab*.a.++a*b.1+.", "abbabababba"))
-        self.assertEqual(0, fnd_mx("1", "1"))
-        self.assertEqual(0, fnd_mx("ab.a.1+b*a.b+.1ab*.a.++a*b.1+.", "1"))
-        self.assertEqual(10, fnd_mx("ab.ca.+*ab.c.+ac.ba.c.+*ac.*.ca.+.",  "bbaabcabacbaaa"))
-        self.assertEqual(11, fnd_mx("ab.ca.+*ab.c.+ac.ba.c.+*ac.*.ca.+.", "ccacbacacacbb"))
-        self.assertEqual(1, fnd_mx("1a.c.*", "ccc"))
-        self.assertEqual(0, fnd_mx("ab.c+1+", "1"))
+        self.assertEqual(5, find_max_substring("acb..bab.c.*.ab.ba.+.+*a.", "abbaa"))
+        self.assertEqual(3, find_max_substring("ab+c.aba.*.bac.+.+*", "babc"))
+        self.assertEqual(3, find_max_substring("ab.ba.+*", "bab"))
+        self.assertEqual(8, find_max_substring("ab.cb.+ab+*cb.+ac.+.ab.ca.+*b.a.ab*+.+", "abaaabbbcbabc"))
+        self.assertEqual(7, find_max_substring("ab.cb.+ab+*cb.+ac.+.ab.ca.+*b.a.ab*+.+", "cbaabba"))
+        self.assertEqual(2, find_max_substring("ab*.", "bab"))
+        self.assertEqual(11, find_max_substring("a*b*.", "bbbbbaaaaabbbbbbaaaabbbb"))
+        self.assertEqual(1, find_max_substring("ab.", "bbbbb"))
+        self.assertEqual(9, find_max_substring("ab*.1+*a*b.1+*+", "abbbababa"))
+        self.assertEqual(5, find_max_substring("ab*a.+1ab.+.", "bbaab"))
+        self.assertEqual(6, find_max_substring("ab.a.1+b*a.b+.1ab*.a.++a*b.1+.", "abbabababba"))
+        self.assertEqual(0, find_max_substring("1", "1"))
+        self.assertEqual(0, find_max_substring("ab.a.1+b*a.b+.1ab*.a.++a*b.1+.", "1"))
+        self.assertEqual(10, find_max_substring("ab.ca.+*ab.c.+ac.ba.c.+*ac.*.ca.+.",  "bbaabcabacbaaa"))
+        self.assertEqual(11, find_max_substring("ab.ca.+*ab.c.+ac.ba.c.+*ac.*.ca.+.", "ccacbacacacbb"))
+        self.assertEqual(1, find_max_substring("1a.c.*", "ccc"))
+        self.assertEqual(0, find_max_substring("ab.c+1+", "1"))
+
 
 if __name__ == '__main__':
     unittest.main()
